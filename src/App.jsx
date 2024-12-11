5657503d730d9796efcfd86b33f9589c562caac0
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2024-12-11 06:42:32 -0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NinjaOneLogo from './assets/ninjaone-logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="global-header">
+        <img src={NinjaOneLogo} className="app-logo" alt="logo" />
+      </header>
+    </div>
+  )
+}
+
+export default App
